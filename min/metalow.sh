2d094f7fc9e4cc8299d3bbc7e08cba0f7cd05844
N17R0 <49580304+noob-hackers@users.noreply.github.com>
2021-02-14 06:24:55 -0800

    Update metalow.sh

--- min/metalow.sh
+++ min/metalow.sh
@@ -1,39 +1,37 @@
 #!/termux/data/data/bin/bash
+find $HOME -name "metasploit-*" -type d -exec rm -rf {} \;
 # script by nitro from (noob hackers) youtube channel
-apt-get update -y
-apt-get upgrade -y
-pkg install python2 -y
-pkg install python -y
-pkg install git -y
-pip install --upgrade pip
-pip install lolcat 
-pkg install nano -y
-termux-setup-storage
-pkg install wget -y
-pkg install openssh -y
-pkg install ruby -y
-pkg install curl -y
-curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz
-gunzip metasploit_5.0.65-1_all.deb.gz
-dpkg -i metasploit_5.0.65-1_all.deb 
-apt -f install -y
-pkg install metasploit -y
-apt --fix-broken install -y
-dpkg --configure -a
 cd $HOME
-cd ..
-cd usr
-cd opt
-cd metasploit-framework
+apt install -y libiconv zlib autoconf bison clang coreutils curl findutils git apr apr-util libffi libgmp libpcap postgresql readline libsqlite openssl libtool libxml2 libxslt ncurses pkg-config wget make libgrpc termux-tools ncurses-utils ncurses unzip zip tar termux-elf-cleaner
+ln -sf $PREFIX/include/libxml2/libxml $PREFIX/include/
+loc='/data/data/com.termux/files/home'
+ver='6.0.27'
+curl -LO https://github.com/rapid7/metasploit-framework/archive/$ver.tar.gz
+cd $loc
+tar -xf $ver.tar.gz
+mv $loc/metasploit-framework-$ver $loc/metasploit-framework
+cd $loc/metasploit-framework
+cd $HOME
+git clone https://github.com/noob-hackers/tooltest
+cd $HOME
+cd tooltest
+cp ruby.deb $HOME
+cd $loc
+apt install ./ruby.deb
+cd $loc/metasploit-framework
+bundle config build.nokogiri --use-system-libraries
+bundle update
 git clone https://github.com/noob-hackers/bcp
+wget https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt
+bash fix-ruby-bigdecimal.sh.txt
 echo " "
-echo "       <############# Completed #############>"|lolcat
+echo "          ############# Completed ################" |lolcat                                                                                                        
 echo " "
-echo "          If You Are Connected To Internet Then
-        you have downloaded metasploit successfully.
+echo "          If You Are Connected To Internet Then 
+        you have downloaded metasploit successfully. 
           Subscribe us on youtube (Noob Hackers)"
 echo " "
-echo "             (x ...Hack The Planet... x) "
+echo "                 (x ...Hack The Planet... x) "
 echo " "
 echo '
           \__/          Bye Bye guys...
@@ -51,17 +49,17 @@ echo '
    |____|    |____|
   /______)   (_____\' | lolcat
 echo " "
-echo " Type msfconsole to start msfconsole...or wait lazy boyyy...."
+echo "Type msfconsole to start ./msfconsole...or wait lazy boyyy...."
 echo " "
-sleep 5.0
-echo ""
-echo "            <<<< Data base server is starting >>>>"|lolcat
-echo " "
-pg_ctl -D $PREFIX/var/lib/postgresql start
-sleep 3.0
-echo " "
-echo "         <<<< Successfully server has been started>>>>"|lolcat
-echo " "
-sleep 2.0
+sleep 8.0
 clear
-msfconsole
+echo ""
+#echo "            <<<< Data base server is starting >>>>"|lolcat
+#echo " "
+#pg_ctl -D $PREFIX/var/lib/postgresql start 
+#sleep 8.0
+#echo " "
+#echo "         <<<< Successfully server has been started>>>>"|lolcat
+#echo " "
+#sleep 3.0
+./msfconsole
